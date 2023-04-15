60e7400630ffcbd29a44761a0212bcfc243f1683
ViktoriiaKhmelevska <116114782+ViktoriiaKhmelevska@users.noreply.github.com>
2023-04-15 19:59:11 +0000

    Deploying to gh-pages from @ ViktoriiaKhmelevska/goit-js-hw-09@9118ca3c46f876034ada32d1d471f23787f8a18a 🚀

---
+++ 01-color-switcher.8a6e94f2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector('button[data-start="Start"]'),o=document.querySelector("dataset.stop");console.log(t),console.log(o)}();
+//# sourceMappingURL=01-color-switcher.8a6e94f2.js.map
