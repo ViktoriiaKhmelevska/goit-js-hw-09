148ccb01886a5e66afac3349baddc9ec24b85e6d
Viktoria Khmelevska <vikahvl@ukr.net>
2023-04-16 00:51:39 +0300

    task1 added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,33 @@
-const startButton = document.querySelector('button[data-start="Start"]');
-const stopButton = document.querySelector('dataset.stop');
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body')
 
-console.log(startButton);
-console.log(stopButton);
+// start.addEventListener('click', startClick);
+// stop.addEventListener('click', stopClick);
+
+// function startClick(e) {
+
+//   setInterval(() => {
+// let color = getRandomHexColor();
+//     body.style.backgroundColor = color;
+//  }, 1000);
+// };
+
+// function stopClick() { 
+
+// };
+start.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    start.disabled = true;
+let color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+});
+
+stop.addEventListener("click", () => {
+    clearInterval(timerId);
+start.disabled = false;
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
