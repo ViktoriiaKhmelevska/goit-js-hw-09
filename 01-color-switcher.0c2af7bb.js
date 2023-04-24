a708dfaa53708280946977970c1bd6aa44793925
ViktoriiaKhmelevska <116114782+ViktoriiaKhmelevska@users.noreply.github.com>
2023-04-24 17:35:49 +0000

    Deploying to gh-pages from @ ViktoriiaKhmelevska/goit-js-hw-09@b1188c00df6221fef4f4b6a8774f3af7622d62e2 🚀

--- 01-color-switcher.0c2af7bb.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){t.disabled=!0;var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.backgroundColor=e}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.0c2af7bb.js.map
---
+++ 01-color-switcher.81212d8e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a;e.disabled=!0,t.addEventListener("click",(()=>{a=setInterval((()=>{t.disabled=!0,e.disabled=!1;let a=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=a}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.81212d8e.js.map
